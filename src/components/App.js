afa01cde10936d6dd8a2b812660b8d9f7c3daf43
宇田川 航 <wataru-udagawa@navitime.co.jp>
2018-11-24 00:34:36 +0900

    [update]カウンターをReduxで実装

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const App = () => <Counter></Counter>
+import { increment, decrement } from '../actions'
 
-class Counter extends Component {
-
-  constructor(props) {
-    super(props)
-
-    this.state = { count: 0 }
-  }
-
-  hundlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  hundleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-
+class App extends Component {
   render() {
+    const props = this.props
+
     return (
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.hundlePlusButton}>+1</button>
-        <button onClick={this.hundleMinusButton}>-1</button>
+        <div>count: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value })
+const mapDispatchToProps = ({ increment, decrement })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
