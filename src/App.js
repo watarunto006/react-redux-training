adaa2d0831520aa2b97219179cbff0e8b94eb9ac
宇田川 航 <wataru-udagawa@navitime.co.jp>
2018-11-23 19:24:59 +0900

    [modify]stateについて

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import PropTypes from'prop-types';
+import React, { Component } from 'react';
 
-const App = () => {
-  const profiles =  [
-    {
-      name: "太郎", age: 10
-    },
-    {
-      name: "花子", age: 5
-    },
-    {
-      name: "NoName"
-    }
-  ]
-  return (<div>
-    {
-      profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />
-      })
-    }
-  </div>)
-}
+const App = () => <Counter></Counter>
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old</div>
-}
+class Counter extends Component {
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+  constructor(props) {
+    super(props)
+
+    this.state = { count: 0 }
+  }
+
+  hundlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  hundleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.hundlePlusButton}>+1</button>
+        <button onClick={this.hundleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
