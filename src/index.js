bcf306ff07f916d54e27d9b0b7b2e84e152ba020
宇田川 航 <wataru-udagawa@navitime.co.jp>
2018-11-24 16:34:53 +0900

    [update]非同期通信をして、テーブルを表示するまで

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-import './index.css';
+import './index.css'
 import reducer from './reducers'
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import EventsIndex from './components/event_index'
+import * as serviceWorker from './serviceWorker'
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <Provider store={ store }>
-        <App />
+    <Provider store={store}>
+        <EventsIndex />
     </Provider>,
     document.getElementById('root')
-);
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
